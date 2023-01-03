73fc0277e0476a1697622082c860cdc122735e38
Juan Pablo Montoya <101369659+jmontoyavallejo@users.noreply.github.com>
2023-01-03 15:33:33 -0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>
+                    As a highly skilled and results-oriented Python back-end developer and data scientist with a strong background in project
+management and engineering, I am confident in my ability to contribute to the success of your team. With my expertise in
+Python, Power BI, and SQL, as well as my proven track record of developing new tools to improve efficiency, I am well-equipped
+to tackle any challenge. In addition, my excellent communication skills and ability to work effectively in a collaborative and
+multicultural environment make me an asset to any project. I am eager to bring my skills and enthusiasm for learning to your
+organization and make a meaningful impact.”
+                </p>
+                <br />
+                <p className='text-xl '>
+                In my experience, I have gained knowledge in JavaScript, HTML, and CSS, as well as expertise in React and Tailwind. I am also proficient in FastAPI, which allows me to effectively deploy and maintain a wide range of applications and challenges. My skills in these technologies have been developed through both professional and personal projects, and I am always eager to learn more and expand my capabilities.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
