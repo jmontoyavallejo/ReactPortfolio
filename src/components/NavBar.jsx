6a5e7b4b91837871aff87885853107967e9b89fc
Juan Pablo Montoya <101369659+jmontoyavallejo@users.noreply.github.com>
2023-09-29 11:23:48 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl  ml-2">Juan Pablo Montoya Vallejo</h1>
+        <h1 className="text-5xl  ml-2 hidden md:flex">Juan Pablo Montoya Vallejo</h1>
       </div>
 
       <ul className="hidden md:flex">
